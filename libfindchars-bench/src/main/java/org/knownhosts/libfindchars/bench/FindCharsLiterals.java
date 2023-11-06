8c0b0df9778ea934b4b564aa355cbb44f3b68946
db <dbgh@knownhosts.org>
2023-11-06 20:31:33 +0100

    reformat templates use zero() instead of 0 broadcast and regenerate engines

--- libfindchars-bench/src/main/java/org/knownhosts/libfindchars/bench/FindCharsLiterals.java
+++ libfindchars-bench/src/main/java/org/knownhosts/libfindchars/bench/FindCharsLiterals.java
@@ -6,5 +6,5 @@ public final class FindCharsLiterals {
     public static final byte WHITESPACES = 65;
     public static final byte PUNCTIATIONS = 71;
     public static final byte PLUS = 101;
-
+    
 }
--- libfindchars-compiler/src/test/java/org/knownhosts/libfindchars/experiments/FindCharsLiterals.java
+++ libfindchars-compiler/src/test/java/org/knownhosts/libfindchars/experiments/FindCharsLiterals.java
@@ -5,8 +5,8 @@ public final class FindCharsLiterals {
     public static final byte STAR = 77;
     public static final byte NUMS = 126;
     public static final byte WHITESPACES = 65;
-    public static final byte COMPARISON = -16;
+    public static final byte COMPARISON = -119;
     public static final byte PUNCTIATIONS = 71;
     public static final byte PLUS = 101;
-
+    
 }
